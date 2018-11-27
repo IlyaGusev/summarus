62af391e8d72618d376d07e85258724c54b18bdf
Ilya Gusev <phoenixilya@gmail.com>
2018-11-28 01:12:25 +0300

    Some new models

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 
-import rouge
+from rouge import Rouge
 from allennlp.data.vocabulary import Vocabulary
-from allennlp.models.encoder_decoders.simple_seq2seq import SimpleSeq2Seq
 from allennlp.common.params import Params
 from allennlp.data.iterators.data_iterator import DataIterator
 from allennlp.training.trainer import Trainer
 from allennlp.models.model import Model
 from allennlp.predictors.simple_seq2seq import SimpleSeq2SeqPredictor
 
+from summarus.seq2seq import Seq2Seq
 from summarus.datasets.cnn_dailymail_reader import CNNDailyMailReader
 
 train_urls = "/data/cnn_dailymail/all_train.txt"
@@ -18,59 +18,77 @@ cnn_dir = "/data/cnn_dailymail/cnn_stories_tokenized"
 dm_dir = "/data/cnn_dailymail/dm_stories_tokenized"
 train_cache = "/data/cnn_dailymail/train.pickle"
 val_cache = "/data/cnn_dailymail/val.pickle"
-reader = CNNDailyMailReader(cnn_dir, dm_dir, lazy=True)
-train_dataset = reader.read(train_urls)
-val_dataset = reader.read(val_urls)
-test_dataset = reader.read(test_urls)
-
-models_path = "models"
-model_path = os.path.join(models_path, "25_11_2018_2")
-vocabulary_path = os.path.join(model_path, "vocabulary")
-params_path = os.path.join(model_path, "config.json")
-
-# train_short_dataset = []
-# for instance in train_dataset:
-#     train_short_dataset.append(instance)
-#     if len(train_short_dataset) == 1000:
-#         break
-# vocabulary = Vocabulary.from_instances(train_dataset, max_vocab_size=50000)
-# vocabulary.save_to_files("vocabulary")
-
-vocabulary = Vocabulary.from_files(vocabulary_path)
-params = Params.from_file(params_path)
-model = SimpleSeq2Seq.from_params(params.pop("model"), vocab=vocabulary, target_namespace="abstract_tokens")
-print(model)
-print("Trainable params count: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
-
-iterator = DataIterator.from_params(params.pop('iterator'))
-iterator.index_with(vocabulary)
-trainer = Trainer.from_params(model, model_path, iterator,
-                              train_dataset, val_dataset, params.pop('trainer'))
-trainer.train()
 
 
-# vocabulary = Vocabulary.from_files(vocabulary_path)
-# params = Params.from_file(params_path)
-# model = Model.load(params, model_path, cuda_device=0)
-#
-# all_hypothesis = []
-# all_references = []
-# predictor = SimpleSeq2SeqPredictor(model, reader)
-# eval_path = os.path.join(model_path, "eval.txt")
-# with open(eval_path, "w", encoding="utf-8") as w:
-#     for article, abstract in reader.parse_files(test_urls):
-#         pred_abstract = " ".join(predictor.predict(article)["predicted_tokens"])
-#         all_hypothesis.append(pred_abstract)
-#         all_references.append(abstract)
-#         w.write(abstract + "\t" + pred_abstract + "\n")
-#
-# evaluator = rouge.Rouge(metrics=['rouge-n', 'rouge-l', 'rouge-w'],
-#                         max_n=4,
-#                         limit_length=True,
-#                         length_limit=100,
-#                         length_limit_type='words',
-#                         stemming=False)
-# scores = evaluator.get_scores(all_hypothesis, all_references)
-# print(scores)
+def train(model_name):
+    models_path = "models"
+    model_path = os.path.join(models_path, model_name)
+    vocabulary_path = os.path.join(model_path, "vocabulary")
+    params_path = os.path.join(model_path, "config.json")
+
+    reader = CNNDailyMailReader(cnn_dir, dm_dir, lazy=True)
+    train_dataset = reader.read(train_urls)
+    val_dataset = reader.read(val_urls)
+
+    if os.path.exists(vocabulary_path):
+        vocabulary = Vocabulary.from_files(vocabulary_path)
+    else:
+        vocabulary = Vocabulary.from_instances(train_dataset)
+        vocabulary.save_to_files(vocabulary_path)
+        train_dataset = reader.read(train_urls)
+
+    params = Params.from_file(params_path)
+    model_params = params.pop("model")
+    model_params.pop("type")
+    model = Seq2Seq.from_params(model_params, vocab=vocabulary)
+    print(model)
+    print("Trainable params count: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    iterator = DataIterator.from_params(params.pop('iterator'))
+    iterator.index_with(vocabulary)
+    trainer = Trainer.from_params(model, model_path, iterator,
+                                  train_dataset, val_dataset, params.pop('trainer'))
+    trainer.train()
 
 
+def evaluate(model_name):
+    models_path = "models"
+    model_path = os.path.join(models_path, model_name)
+    params_path = os.path.join(model_path, "config.json")
+    eval_path = os.path.join(model_path, "eval.txt")
+
+    reader = CNNDailyMailReader(cnn_dir, dm_dir, lazy=True)
+
+    params = Params.from_file(params_path)
+    model = Model.load(params, model_path, cuda_device=0)
+    print(model)
+    print("Trainable params count: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    all_hypothesis = []
+    all_references = []
+
+    if not os.path.exists(eval_path):
+        predictor = SimpleSeq2SeqPredictor(model, reader)
+        with open(eval_path, "w", encoding="utf-8") as w:
+            for article, abstract in reader.parse_files(test_urls):
+                abstract = " ".join(abstract.split(" ")[:100])
+                print("Article: ", article)
+                print("Abstract: ", abstract)
+                pred_abstract = " ".join(predictor.predict(article)["predicted_tokens"])
+                print("Pred abstract: ", pred_abstract)
+                all_hypothesis.append(pred_abstract)
+                all_references.append(abstract)
+                w.write(abstract + "\t" + pred_abstract + "\n")
+    else:
+        with open(eval_path, "r", encoding="utf-8") as r:
+            for line in r:
+                abstract, pred_abstract = line.split("\t")
+                all_hypothesis.append(pred_abstract)
+                all_references.append(abstract)
+
+    evaluator = Rouge()
+    scores = evaluator.get_scores(all_hypothesis, all_references, avg=True)
+    print(scores)
+
+
+evaluate("single_layer_lstm_adam")
