eb5e41e5b8271de194de6473fab9cd783904eb89
Ilya Gusev <phoenixilya@gmail.com>
2019-02-02 11:35:12 +0300

    RIA reader

---
+++ summarus/readers/ria_reader.py
@@ -0,0 +1,41 @@
+import json
+from typing import Dict
+
+from bs4 import BeautifulSoup
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.tokenizers.tokenizer import Tokenizer
+from allennlp.data.token_indexers.token_indexer import TokenIndexer
+from allennlp.data.tokenizers import WordTokenizer
+from allennlp.data.tokenizers.word_splitter import SimpleWordSplitter
+
+from summarus.readers.summarization_reader import SummarizationReader
+
+
+@DatasetReader.register("ria")
+class RIAReader(SummarizationReader):
+    def __init__(self,
+                 tokenizer: Tokenizer = None,
+                 source_token_indexers: Dict[str, TokenIndexer] = None,
+                 target_token_indexers: Dict[str, TokenIndexer] = None,
+                 source_max_tokens: int = 400,
+                 target_max_tokens: int = 100,
+                 separate_namespaces: bool = False) -> None:
+        if not tokenizer:
+            tokenizer = WordTokenizer(word_splitter=SimpleWordSplitter())
+        super().__init__(
+            tokenizer=tokenizer,
+            source_token_indexers=source_token_indexers,
+            target_token_indexers=target_token_indexers,
+            source_max_tokens=source_max_tokens,
+            target_max_tokens=target_max_tokens,
+            separate_namespaces=separate_namespaces
+        )
+
+    def parse_set(self, path):
+        with open(path, "r", encoding="utf-8") as r:
+            for line in r:
+                data = json.loads(line.strip())
+                title = data["title"]
+                text = data["text"]
+                clean_text = BeautifulSoup(text, "lxml").text
+                yield clean_text, title
--- summarus/tests/test_readers.py
+++ summarus/tests/test_readers.py
@@ -2,8 +2,8 @@ import unittest
 
 from allennlp.common.util import START_SYMBOL, END_SYMBOL
 
-from summarus.readers import CNNDailyMailReader
-from summarus.settings import TEST_URLS_FILE, TEST_STORIES_DIR
+from summarus.readers import CNNDailyMailReader, RIAReader
+from summarus.settings import TEST_URLS_FILE, TEST_STORIES_DIR, RIA_EXAMPLE_FILE
 
 
 class TestReaders(unittest.TestCase):
@@ -18,3 +18,10 @@ class TestReaders(unittest.TestCase):
             self.assertEqual(sample.fields["target_tokens"][0].text, START_SYMBOL)
             self.assertEqual(sample.fields["target_tokens"][-1].text, END_SYMBOL)
             self.assertGreater(len(sample.fields["target_tokens"]), 2)
+
+    def test_ria_reader(self):
+        reader = RIAReader()
+        dataset = reader.read(RIA_EXAMPLE_FILE)
+        for sample in dataset:
+            print(sample.fields["source_tokens"])
+            print(sample.fields["target_tokens"])
