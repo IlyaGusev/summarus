658c456f95c6f585191dee38d2a4de4f71e2fbdf
IlyaGusev <phoenixilya@gmail.com>
2020-05-04 15:57:12 +0300

    train_subword_model fix

--- summarus/util/train_subword_model.py
+++ summarus/util/train_subword_model.py
@@ -7,14 +7,17 @@ from allennlp.common.params import Params
 from allennlp.data.dataset_readers import DatasetReader
 
 from summarus.readers import *
-   
 
-def train_subwords(train_path, model_path, model_type, vocab_size, config_path):
+
+def train_subwords(train_path, model_path, model_type,
+                   vocab_size, config_path, lowercase=False):
     temp = tempfile.NamedTemporaryFile(mode="w", delete=False)
     params = Params.from_file(config_path)
-    reader_params = params.pop("reader", default=Params({}))
+    reader_params = params.pop("dataset_reader", default=Params({}))
     reader = DatasetReader.from_params(reader_params)
     for text, summary in reader.parse_set(train_path):
+        text = text.lower() if lowercase else text
+        summary = summary.lower() if lowercase else summary
         temp.write(text + "\n")
         temp.write(summary + "\n")
     temp.close()
@@ -36,5 +39,6 @@ if __name__ == "__main__":
     parser.add_argument('--model-type', type=str, default="bpe")
     parser.add_argument('--vocab-size', type=int, default=50000)
     parser.add_argument('--config-path', type=str, required=True)
+    parser.add_argument('--lowercase', action='store_true')
     args = parser.parse_args()
     train_subwords(**vars(args))
