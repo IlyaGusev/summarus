52fe67b23214b3ceb3b857970205554e1547e691
Ilya Gusev <phoenixilya@gmail.com>
2020-06-14 23:27:12 +0300

    fix train.sh

--- external/bart_scripts/train.sh
+++ external/bart_scripts/train.sh
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0 python3 "${FAIRSEQ_PATH}/train.py" "${DATA_BIN_PATH}" \
     --encoder-normalize-before --decoder-normalize-before \
     --arch mbart_large \
     --task translation_from_pretrained_bart \
-    --langs ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN \ 
+    --langs ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN \
     --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 5 \
     --restore-file "${BART_PATH}/model.pt" \
     --max-tokens $MAX_TOKENS \
