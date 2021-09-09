4cdbb8d8001a594df0e4d404c632a1c6627511c2
Ilya Gusev <phoenixilya@gmail.com>
2021-09-09 13:04:01 +0000

    preprocess.sh fix

--- external/bart_scripts/preprocess.sh
+++ external/bart_scripts/preprocess.sh
@@ -3,8 +3,8 @@ TASK="$2"
 
 rm -rf "${TASK}-bin/"
 fairseq-preprocess \
-    --source-lang "source" \
-    --target-lang "target" \
+    --source-lang "en_XX" \
+    --target-lang "ru_RU" \
     --trainpref "${TASK}/train.bpe" \
     --validpref "${TASK}/val.bpe" \
     --testpref "${TASK}/test.bpe" \
